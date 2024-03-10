5ee02eb4ca84a281dfc021c6112f6ada06059a27
3rickDJ <erick@makingdevs.com>
2024-03-10 13:27:58 -0600

    add packages dnd and mui

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+export default function Welcome() {
+  return (
+    <>
+    <div className="wrapper">
+    <h1>Hola Liz</h1>
+    <h1>Como estas</h1>
+    <h1>Bien?</h1>
+    <p>This is going to be the coolest app in the world!</p>
+    </div>
+    </>
+  );
+}
