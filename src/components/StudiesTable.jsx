a5b5fe116c3f2365b268dc4ecbf69093aba7af7a
3rickDJ <erick.dejesus1@alumno.buap.mx>
2024-04-07 12:46:25 -0600

    delete no used components

--- src/components/StudiesTable.jsx
+++ src/components/StudiesTable.jsx
@@ -5,70 +5,54 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { IconButton, Stack } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import InfoIcon from '@mui/icons-material/Info';
 
-export default function BasicTable() {
-  const rows = [
-    {
-      name: 'Frozen yoghurt',
-      calories: 159,
-      fat: 6.0,
-      carbs: 24,
-      protein: 4.0,
-    },
-    {
-      name: 'Ice cream sandwich',
-      calories: 237,
-      fat: 9.0,
-      carbs: 37,
-      protein: 4.3,
-    },
-    {
-      name: 'Eclair',
-      calories: 262,
-      fat: 16.0,
-      carbs: 23,
-      protein: 6.0,
-    },
-    {
-      name: 'Cupcake',
-      calories: 305,
-      fat: 3.7,
-      carbs: 67,
-      protein: 4.3,
-    },
-    {
-      name: 'Gingerbread',
-      calories: 356,
-      fat: 16.0,
-      carbs: 49,
-      protein: 3.9,
-    },
-  ]
+export default function StudiesTable(props) {
+
+  const { options, testHashById, studies, setStudies } = props;
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>ID</TableCell>
+            <TableCell align="right">Código</TableCell>
+            <TableCell align="right">Clave</TableCell>
+            <TableCell align="right">Título</TableCell>
+            <TableCell align="right">Tarifa</TableCell>
+            <TableCell align="right">Precio</TableCell>
+            <TableCell align="right">Acciones</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {studies.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
+              <TableCell component="th" scope="row">{row.id} </TableCell>
+              <TableCell align="right">{row.type}</TableCell>
+              <TableCell align="right">{row.code}</TableCell>
+              <TableCell align="right">{row.clave}</TableCell>
+              <TableCell align="right">{row.title}</TableCell>
+              <TableCell align="right">{row.pickedFeeConfig}</TableCell>
+              <TableCell align="right">{JSON.stringify(row.feeConfigurations)}</TableCell>
+              <TableCell align="right">
+                <Stack direction="row" spacing={2}>
+                  <IconButton aria-label="edit">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                  <IconButton aria-label="info">
+                    <InfoIcon />
+                  </IconButton>
+                </Stack>
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -76,3 +60,12 @@ export default function BasicTable() {
     </TableContainer>
   );
 }
+
+import PropTypes from 'prop-types';
+
+StudiesTable.propTypes = {
+  options: PropTypes.array.isRequired,
+  testHashById: PropTypes.array.isRequired,
+  studies: PropTypes.array.isRequired,
+  setStudies: PropTypes.func.isRequired,
+};
